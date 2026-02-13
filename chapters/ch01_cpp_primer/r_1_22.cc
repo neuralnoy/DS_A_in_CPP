3cc06b42f493c33f23736c53f4dbef6bc92d7bdb
Arsen Stepanyan <ar.step@icloud.com>
2026-02-13 23:47:10 +0100

    optimal solution to the problem 22 in chapter 1

--- chapters/ch01_cpp_primer/r_1_22.cc
+++ chapters/ch01_cpp_primer/r_1_22.cc
@@ -1,3 +1,24 @@
+/* Write a short C++ function that takes a positive double value x and returns
+the number of times we can divide x by 2 before we get a number less
+than 2. */
+
+#include <cmath>
+#include <iostream>
+
+int CountDivisions(double x) {
+  return (x < 2.0) ? 0 : static_cast<int>(std::log2(x));
+}
+
 int main() {
-  // dummy
+  double num1{10};
+  double num2{51};
+  double num3{112};
+
+  std::cout << "We can divide " << num1 << " by 2 " << CountDivisions(num1)
+            << " times.\n";
+  std::cout << "We can divide " << num2 << " by 2 " << CountDivisions(num2)
+            << " times.\n";
+  std::cout << "We can divide " << num3 << " by 2 " << CountDivisions(num3)
+            << " times.\n";
+  return 0;
 }
