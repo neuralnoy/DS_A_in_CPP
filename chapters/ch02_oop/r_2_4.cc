9e48d4da9e28d7948cab8af04ffd1ad25f75260d
Arsen Stepanyan <ar.step@icloud.com>
2026-02-19 17:36:10 +0100

    add solution to problem 4 chapter 2

---
+++ chapters/ch02_oop/r_2_4.cc
@@ -0,0 +1,13 @@
+// Give an example of a software application where adaptability can mean
+// the difference between a prolonged sales lifetime and bankruptcy.
+//
+// Answer:
+//
+// A mobile operating system that fails to adapt to new hardware and app-centric
+// trends can rapidly lose market share and collapse, while adaptable platforms
+// sustain long-term success (BlackBerry OS vs Android OS).
+
+int main() {
+  // dummy main
+  return 0;
+}
