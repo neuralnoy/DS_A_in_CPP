da12190e1ff21eda6ea9a3f084858781d4f5df5c
Arsen Stepanyan <ar.step@icloud.com>
2026-02-19 17:26:24 +0100

    add solution to problem 3, chapter 2

---
+++ chapters/ch02_oop/r_2_3.cc
@@ -0,0 +1,10 @@
+// Give three examples of life-critical software applications.
+//
+// 1. Medical software
+// 2. Airplane steering system software
+// 3. Car autopiloting software
+
+int main() {
+  // dummy main
+  return 0;
+}
