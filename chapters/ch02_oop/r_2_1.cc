c47ad169a5ed0b6530060eb2ebc3d18e5bb18440
Arsen Stepanyan <ar.step@icloud.com>
2026-02-19 16:58:59 +0100

    adjust Cmake for the second chapter

---
+++ chapters/ch02_oop/r_2_1.cc
@@ -0,0 +1,9 @@
+// What are some potential efficiency disadvantages of having very deep in-
+// heritance trees, that is, a large set of classes, A, B, C, and so on, such
+// that B extends A, C extends B, D extends C, etc.?
+//
+// Answer:
+
+int main() {
+  // dummy main for the Cmake
+}
