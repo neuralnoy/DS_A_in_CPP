e547f2399c207889777d309ad4cad65ae064a50e
Arsen Stepanyan <ar.step@icloud.com>
2026-02-21 17:25:44 +0100

    solution to the 15th problem in chapter 2

--- chapters/ch02_oop/r_2_15.cc
+++ chapters/ch02_oop/r_2_15.cc
@@ -1,1 +1,25 @@
-int main() { return 0; }
+// Write a short C++ function that counts the number of vowels in a given
+// character string.
+
+#include <iostream>
+#include <string>
+
+int CountVowels(const std::string &str) {
+  int count = 0;
+  for (char c : str) {
+    char lower = std::tolower(c);
+    if (lower == 'a' || lower == 'e' || lower == 'i' || lower == 'o' ||
+        lower == 'u') {
+      count++;
+    }
+  }
+  return count;
+}
+
+int main() {
+  std::string text =
+      "ThisIsAwordwhichcontainsprobabySeveralvowelsUniqueOrnotWhoknowsWhat";
+  int totalVowels = CountVowels(text);
+  std::cout << "Total number of vowels: " << totalVowels << "\n";
+  return 0;
+}
