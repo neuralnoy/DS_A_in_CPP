f33b365bb8e497c3a9f959cd105321df2dda4de1
Arsen Stepanyan <ar.step@icloud.com>
2026-03-24 11:45:10 +0100

    add partial solution to the problem 3.2.2, tbc

---
+++ chapters/ch03_arr_lst_recursion/r_3_2.cc
@@ -0,0 +1,29 @@
+// Suppose that two entries of an array A are equal to each other. After run-
+// ning the insertion-sort algorithm of Code Fragment 3.7, will they appear
+// in the same relative order in the final sorted order or in reverse order?
+// Explain your answer.
+
+#include <iostream>
+
+void InsertionSort(double *A, int n) {
+  for (int i = 1; i < n; i++) {
+    double cur = A[i];
+    int j = i - 1;
+
+    while (j >= 0 && A[j] > cur) {
+      A[j + 1] = A[j];
+      j--;
+    }
+
+    A[j + 1] = cur;
+  }
+}
+
+int main() {
+  double someArrayOfDoubles[] = {1.0, 5.4, 3.2, 3.2, 5.5, 1000.3, 0.2, 0.0001};
+
+  InsertionSort(someArrayOfDoubles, 8);
+
+  for (double x : someArrayOfDoubles)
+    std::cout << x << " ";
+}
