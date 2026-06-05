139150d257bde4c080bd4c09a879f32ce958bca0
Arsen Stepanyan <ar.step@icloud.com>
2026-06-05 15:52:28 +0200

    Replace iostream with cstdio in quine example

--- chapters/ch02_oop/c_2_1.cc
+++ chapters/ch02_oop/c_2_1.cc
@@ -2,7 +2,7 @@
 
 int main() {
     const char* s =
-        "#include <iostream>%cint main() {%c    const char *s = %c%s%c;%c    "
+        "#include <cstdio>%cint main() {%c    const char *s = %c%s%c;%c    "
         "printf(s, 10, 10, 34, s, 34, 10, 10);%c    return 0;%c}";
     printf(s, 10, 10, 34, s, 34, 10, 10);
     return 0;
