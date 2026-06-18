3832dcfd52338467445af29055aaba570d677c16
Arsen Stepanyan <ar.step@icloud.com>
2026-06-18 17:02:26 +0200

    Add solution for exercise 4.10

---
+++ chapters/ch04_analysis_tools/r_4_10.cc
@@ -0,0 +1,25 @@
+// Explain why the plot of the function nc is a straight line with slope c on a
+// log-log scale.
+//
+//
+// Solution:
+//
+// On a log-log plot we plot transformed coordinates:
+//    X = log(n), Y = log(y)
+//
+// Given the function:
+//    y = n^c
+//
+// Take logarithm on both sides:
+//    log(y) = log(n^c)
+//
+// Use log rule:
+//    log(n^c) = c * log(n)
+//
+// So:
+//    Y = c * X
+//
+// This is a linear equation in (X, Y) space,
+// hence the graph is a straight line with slope c on a log-log plot.
+
+int main() { return 0; }
