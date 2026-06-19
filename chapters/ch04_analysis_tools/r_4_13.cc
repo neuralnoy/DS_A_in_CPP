57e15a2fabcfb62e9492dd9848176ef44612e043
Arsen Stepanyan <ar.step@icloud.com>
2026-06-19 12:47:57 +0200

    Add exercise R-4.13 to analysis tools chapter

---
+++ chapters/ch04_analysis_tools/r_4_13.cc
@@ -0,0 +1,23 @@
+// Order the following functions by asymptotic growth rate.
+// 4n(log n) + 2n
+// 2^10
+// 2^(log n)
+// 3n + 100log n
+// 4n
+// 2^n
+// n^2 + 10n
+// n^3
+// n log n
+//
+// Solution fastest to slowest:
+// 1. 2^10 (constant time)
+// 2. 2^(log n) (linear time, equal to n assuming log is base 2)
+// 3. 3n + 100log n
+// 4. 4n
+// 5. n log n
+// 6. 4n(log n) + 2n
+// 7. n^2 + 10n
+// 8. n^3
+// 9. 2^n
+
+int main() { return 0; }
