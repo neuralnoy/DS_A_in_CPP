2d3867e1904ba4d1f9e424d7eab393ff8a76e8f3
Arsen Stepanyan <ar.step@icloud.com>
2026-06-21 12:27:50 +0200

    Add complexity analysis to r_4_17.cc

--- chapters/ch04_analysis_tools/r_4_17.cc
+++ chapters/ch04_analysis_tools/r_4_17.cc
@@ -8,5 +8,11 @@
 // for i ← 2 to n−1 by increments of 2 do
 // s ← s + A[i]
 // return s
+//
+// Solution:
+// Assuming that accessing an array element takes O(1) time,
+// and that addition and assignment are also O(1) operations,
+// the loop will perform approximately n/2 constant-time operations.
+// Therefore, the overall running time of the algorithm is O(n).
 
 int main() { return 0; }
