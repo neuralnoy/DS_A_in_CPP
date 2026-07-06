06ab966601ba6df830c7f8cd142970a76c1742c2
Arsen Stepanyan <ar.step@icloud.com>
2026-07-06 11:39:04 +0200

    Implement array reversal logic in r_6_1.cc

--- chapters/ch06_list_and_iterator/r_6_1.cc
+++ chapters/ch06_list_and_iterator/r_6_1.cc
@@ -1,1 +1,33 @@
-int main() { return 0; }
+// Give a C++ code fragment for reversing an array.
+
+#include <array>
+#include <iostream>
+#include <utility>
+
+void revertArray(std::array<int, 16>& some_arr) {
+    size_t j = some_arr.size() - 1;
+    for (size_t i = 0; i < j; i++) {
+        std::swap(some_arr[i], some_arr[j]);
+        j--;
+    }
+}
+
+int main() {
+    std::array<int, 16> arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
+
+    std::cout << "Original array:\n";
+    for (int val : arr) {
+        std::cout << val << " ";
+    }
+    std::cout << "\n\n";
+
+    revertArray(arr);
+
+    std::cout << "Reversed array:\n";
+    for (int val : arr) {
+        std::cout << val << " ";
+    }
+    std::cout << "\n";
+
+    return 0;
+}
