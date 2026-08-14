30fea67f98828d3f3c816e5b94df98775d7975b0
Arsen Stepanyan <ar.step@icloud.com>
2026-08-14 16:28:37 +0200

    Add solution for exercise 7.15 for drawing a tree for preorder and inorder traversal

---
+++ chapters/ch07_trees/r_7_15.cc
@@ -0,0 +1,17 @@
+// Draw a (single) binary tree T , such that:
+// • Each internal node of T stores a single character
+// • A preorder traversal of T yields EXAMFUN
+// • An inorder traversal of T yields MAFXUEN
+//
+// Solution:
+//
+//         E
+//        / \
+//       X   N
+//      / \
+//     A   U
+//    / \
+//   M   F
+//
+
+int main() { return 0; }
